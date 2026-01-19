5c301deb9aad7c79e7302dda4d622448ecbc4bca
DanObermiller <charrcorncob@gmail.com>
2026-01-18 23:37:28 -0600

    Update ObermillerWeek1AssignmentSaturday.java

--- ObermillerWeek1AssignmentSaturday.java
+++ ObermillerWeek1AssignmentSaturday.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.testing;
+package com.mycompany.ObermillerWeek1AssignmentSaturday;
 
 import java.util.ArrayList;
 import java.util.Scanner;
